9eba155f73644f26dccdc92c1eadae620fe490fc
coldy <coldys_@hotmail.com>
2023-07-11 19:34:18 +0100

    update more exercises with 02. Take/Skip Rope

--- more_exercises/05_list_advanced/02_take_skip_rope.py
+++ more_exercises/05_list_advanced/02_take_skip_rope.py
@@ -2,6 +2,7 @@ user_input = input()
 
 digits = []
 letter = ''
+
 for ch in user_input:
     if ch.isnumeric():
         digits.append(int(ch))
@@ -10,6 +11,7 @@ for ch in user_input:
 
 take_digit = []
 skip_digits = []
+
 for i in range(len(digits)):
     if i % 2 == 0:
         take_digit.append(digits[i])
@@ -17,6 +19,7 @@ for i in range(len(digits)):
         skip_digits.append(digits[i])
 
 result = ''
+
 for i in range(len(take_digit)):
     result += letter[:take_digit[i]]
     letter = letter[take_digit[i]:]
