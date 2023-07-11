301b9f93d49d3e02bd2658e170f2e86b8c5ff340
coldy <coldys_@hotmail.com>
2023-07-11 19:33:39 +0100

    update more exercises with 03. Kate's Way Out

---
+++ more_exercises/05_list_advanced/02_take_skip_rope.py
@@ -0,0 +1,25 @@
+user_input = input()
+
+digits = []
+letter = ''
+for ch in user_input:
+    if ch.isnumeric():
+        digits.append(int(ch))
+    else:
+        letter += ch
+
+take_digit = []
+skip_digits = []
+for i in range(len(digits)):
+    if i % 2 == 0:
+        take_digit.append(digits[i])
+    else:
+        skip_digits.append(digits[i])
+
+result = ''
+for i in range(len(take_digit)):
+    result += letter[:take_digit[i]]
+    letter = letter[take_digit[i]:]
+    letter = letter[skip_digits[i]:]
+
+print(result)
